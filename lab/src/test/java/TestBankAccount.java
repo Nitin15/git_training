80d2e3ce4c80533590421fca136c76d12d8828b4
Nitin15 <test@gmail.com>
2016-01-04 16:16:18 +0530

    Jacoco testing

--- lab/src/test/java/TestBankAccount.java
+++ lab/src/test/java/TestBankAccount.java
@@ -6,9 +6,16 @@ import org.junit.Test;
 
 public class TestBankAccount {
     @Test
-    public void testFunds() {
+    public void testDebitWithSufficientFunds() {
        BankAccount account = new BankAccount(10);
        double amount = account.debit(5);
        Assert.assertEquals(5.0, amount);
+    }
+
+    @Test
+    public void testDebitWithInSufficientFunds() {
+       BankAccount account = new BankAccount(10);
+       double amount = account.debit(11);
+       Assert.assertEquals(10.0, amount);
     }
 }
