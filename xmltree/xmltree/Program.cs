83e7dd2dce648e62a583fb3451a72081c0f0904c
nzodpe <nzodpe@tibco.com>
2017-09-21 20:32:21 +0530

    jenkins eg

---
+++ xmltree/xmltree/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using System.Xml.Linq;
+using System.Linq;
+using System;
+class Program
+{
+    static XElement CreateFileSystemXmlTree(string source)
+    {
+        System.IO.DirectoryInfo di = new DirectoryInfo(source);
+        return new XElement("Dir",
+            new XAttribute("Name", di.Name),
+            from d in Directory.GetDirectories(source)
+            select CreateFileSystemXmlTree(d),
+            from fi in di.GetFiles()
+            select new XElement("File",
+                new XElement("Name", fi.Name),
+                new XElement("Length", fi.Length)
+            )
+        );
+    }
+
+    static void Main(string[] args)
+    {
+        XElement fileSystemTree = CreateFileSystemXmlTree(args[0]);
+        //"E:\\P4\\depot\\ReleaseQA\\LoyaltySystem\\Development\\TIBCO\\Deployment\\RCS"
+        Console.WriteLine(fileSystemTree);
+        Console.WriteLine("------");
+        long totalFileSize =
+            (from f in fileSystemTree.Descendants("File")
+             select (long)f.Element("Length")).Sum();
+        Console.WriteLine("Total File Size:{0}", totalFileSize);
+    }
+}
